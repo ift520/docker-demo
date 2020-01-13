ac9739413389bfa4749e54d490b6ca0b5dc9da42
ift <1987047448@qq.com>
2020-01-13 17:24:51 +0800

    修改TestController

--- src/main/java/com/ift/dockerdemo/controller/TestController.java
+++ src/main/java/com/ift/dockerdemo/controller/TestController.java
@@ -1,8 +1,13 @@
 package com.ift.dockerdemo.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author liufei
  * @since 2020-01-13 17:12
  */
+@RestController
+@RequestMapping("/a")
 public class TestController {
 }
