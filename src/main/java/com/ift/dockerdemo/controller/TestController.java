d29a56d972973b31fdd2bd6b3c97c64a54476484
ift520 <liu.ift@gmail.com>
2020-01-13 19:27:38 +0800

    增加测试接口/a

--- src/main/java/com/ift/dockerdemo/controller/TestController.java
+++ src/main/java/com/ift/dockerdemo/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.ift.dockerdemo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/a")
 public class TestController {
+
+    @GetMapping
+    public String test(){
+        return "test";
+    }
 }
