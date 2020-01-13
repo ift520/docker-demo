48536b873c9bfad3ee01f41782e7473de5db6692
ift <1987047448@qq.com>
2020-01-13 15:36:38 +0800

    初始化

---
+++ src/main/java/com/ift/dockerdemo/DockerDemoApplication.java
@@ -0,0 +1,25 @@
+package com.ift.dockerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+/**
+ * @author liufei
+ */
+@SpringBootApplication
+@RestController
+public class DockerDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerDemoApplication.class, args);
+    }
+
+    @GetMapping("/test")
+    public String test() {
+        return UUID.randomUUID().toString();
+    }
+}
---
+++ src/test/java/com/ift/dockerdemo/DockerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ift.dockerdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
